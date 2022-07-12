6a7d1d390b4e3e2f81c19deea8d0fe076bdf097f
Varun S Athreya <varun.sathreya@gmail.com>
2022-07-12 23:51:14 +0530

    feat: Project Init

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	router.Run(":" + port)
+
+}
