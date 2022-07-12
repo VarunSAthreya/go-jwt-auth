6f4909b538bca6053caa24e5439d293453f38dfc
Varun S Athreya <varun.sathreya@gmail.com>
2022-07-13 01:15:30 +0530

    Added DB connection and godotenv pkg

---
+++ database/databaseConnection.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBInstance() *mongo.Client {
+	err := godotenv.Load("../.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	MONGO_URL := os.Getenv("MONGO_URL")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MONGO_URL))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
