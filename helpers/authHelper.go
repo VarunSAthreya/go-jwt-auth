d21bb25e6c706e557c145ed349b654ae05bad4fb
Varun S Athreya <varun.sathreya@gmail.com>
2022-07-15 22:07:30 +0530

    Added function to check user type

---
+++ helpers/authHelper.go
@@ -0,0 +1,33 @@
+package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	err = nil
+
+	if userType != role {
+		err = errors.New("User type is not correct")
+		return err
+	}
+
+	return err
+}
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil
+
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorized to access the data!")
+		return err
+	}
+	err = CheckUserType(c, userType)
+
+	return err
+}
