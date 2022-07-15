9752c4b67d5a46dbe2f544e1e6c4ddac4de8e912
Varun S Athreya <varun.sathreya@gmail.com>
2022-07-15 22:07:59 +0530

    Added signup handler

--- controllers/userController.go
+++ controllers/userController.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -21,7 +22,79 @@ func HashPassword()
 
 func VerifyPassword()
 
-func Signup()
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while checking Email."})
+
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Email already exists."})
+
+			return
+		}
+
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while checking Phone number."})
+
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Phone Number already exists."})
+
+			return
+		}
+
+		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		user.ID = primitive.NewObjectID()
+		user.User_id = user.ID.Hex()
+		token, refreshToken, tokenErr := helpers.GenerateTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
+
+		if tokenErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while generating tokens."})
+
+			return
+		}
+
+		user.Token = &token
+		user.Refresh_token = &refreshToken
+
+		resultInsertNumber, insertErr := userCollection.InsertOne(ctx, user)
+
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while inserting user."})
+
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK, resultInsertNumber)
+	}
+}
 
 func Signin()
 
