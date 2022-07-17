2d23c865f17ec81185684c12ceaf6205eeab94f1
Varun S Athreya <varun.sathreya@gmail.com>
2022-07-17 17:26:51 +0530

    Added Authenticate middleware

---
+++ middleware/authMiddleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/VarunSAthreya/go-jwt/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("Authorization")
+
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No Authorization header provided."})
+			c.Abort()
+
+			return
+		}
+
+		claims, msg := helpers.ValidateToken(clientToken)
+		if msg != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			c.Abort()
+
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_name)
+		c.Set("last_name", claims.Last_name)
+		c.Set("uid", claims.Uid)
+		c.Set("user_type", claims.User_type)
+
+		c.Next()
+	}
+}
